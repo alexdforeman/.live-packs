a3c2d37d6dfede752b0a03fd661d877f3fea0adf
alexdforeman <alexdforeman@gmail.com>
2013-10-15 15:08:28 +0100

    Added twitter mode as I didnt realise I didnt have it at work

--- alexdforeman-pack/init.el
+++ alexdforeman-pack/init.el
@@ -6,8 +6,16 @@
 ;; Load bindings config
 (live-load-config-file "bindings.el")
 
-;; Load Setup config
-(live-load-config-file "setup.el")
-
 ;; Load cuda mode config: http://www.emacswiki.org/emacs/CudaMode
 (live-load-config-file "cuda_mode.el")
+
+(live-load-config-file "twittering-mode.el")
+
+(live-load-config-file "setup.el")
+
+;;; Twitter Setup 'M-x twit'
+(require 'twittering-mode)
+(setq twittering-use-master-password t)
+(setq twittering-icon-mode t)
+(setq twittering-timer-interval 60)
+(twittering-icon-mode)

01603bed507654b4c831f235c8dff4a31a59a370
alexdforeman <alexdforeman@gmail.com>
2013-10-21 15:03:21 +0100

    Added groovy mode

--- alexdforeman-pack/init.el
+++ alexdforeman-pack/init.el
@@ -19,3 +19,29 @@
 (setq twittering-icon-mode t)
 (setq twittering-timer-interval 60)
 (twittering-icon-mode)
+
+;;; Groovy Mode
+(live-load-config-file "grails-mode.el")
+(live-load-config-file "groovy-mode.el")
+(live-load-config-file "groovy-electric.el")
+(live-load-config-file "inf-groovy.el")
+
+;;; turn on syntax highlighting
+(global-font-lock-mode 1)
+
+;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+;;; make Groovy mode electric by default.
+(add-hook 'groovy-mode-hook
+          '(lambda ()
+             (require 'groovy-electric)
+                          (groovy-electric-mode)))
+
+
+;;; JDEE for Java
+(add-to-list 'load-path "~/Programs/jdee-2.4.1/lisp")
+(load "jde")

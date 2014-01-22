f54427493fa5dabe4157701ad74c6328dfa9a44e
alexdforeman <@gmail.comalexdforeman>
2014-01-21 19:21:21 -0500

    Removed eclim mode'

--- alexdforeman-pack/init.el
+++ alexdforeman-pack/init.el
@@ -49,25 +49,3 @@
           '(lambda ()
              (require 'groovy-electric)
                           (groovy-electric-mode)))
-
-
-
-;;; emacs-eclim
-(require 'eclim)
-(global-eclim-mode)
-(require 'eclimd)
-(setq help-at-pt-display-when-idle t)
-(setq help-at-pt-timer-delay 0.1)
-(help-at-pt-set-timer)
-;; regular auto-complete initialization
-(require 'auto-complete-config)
-(ac-config-default)
-
-;; add the emacs-eclim source
-(require 'ac-emacs-eclim-source)
-(ac-emacs-eclim-config)
-
-(require 'company)
-(require 'company-emacs-eclim)
-(company-emacs-eclim-setup)
-(global-company-mode t)
